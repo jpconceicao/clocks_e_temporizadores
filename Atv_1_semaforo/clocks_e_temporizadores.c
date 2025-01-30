8776873f73f8f6567d7cd1d5c879890cb48d002c
Jorge Palma <jorge.palma@fieb.org.br>
2025-01-30 18:01:14 -0300

    Corrigindo pinos dos leds na simulação e melhorando código

--- Atv_1_semaforo/clocks_e_temporizadores.c
+++ Atv_1_semaforo/clocks_e_temporizadores.c
@@ -20,31 +20,18 @@ typedef enum {
 estado_t estado_atual = VERMELHO;
 
 bool funcaro_de_repeticao_callback();
+void setup_gpios();
 
 int main()
 {
     stdio_init_all();
-
-    gpio_init(RED_LED_PIN);
-    gpio_set_dir(RED_LED_PIN, GPIO_OUT);
-    gpio_init(YELLOW_LED_PIN);
-    gpio_set_dir(YELLOW_LED_PIN, GPIO_OUT);
-    gpio_init(GREEN_LED_PIN);
-    gpio_set_dir(GREEN_LED_PIN, GPIO_OUT);
-
-    gpio_init(RED_RGB);
-    gpio_set_dir(RED_RGB, GPIO_OUT);
-    gpio_init(GREEN_RGB);
-    gpio_set_dir(GREEN_RGB, GPIO_OUT);
-    gpio_init(BLUE_RGB);
-    gpio_set_dir(BLUE_RGB, GPIO_OUT);
+    setup_gpios();
 
     struct repeating_timer timer;
 
     add_repeating_timer_ms(3000, funcaro_de_repeticao_callback, NULL, &timer);
 
-    gpio_put(RED_LED_PIN, 1);
-    gpio_put(RED_RGB, 1);
+    
 
     while (true) {
         sleep_ms(9000);
@@ -96,6 +83,32 @@ bool funcaro_de_repeticao_callback(struct repeating_timer *t)
 
             estado_atual = VERMELHO;
             break;
+        default:
+            break;
     }
     return true; // Retornando `true` mantém o timer ativo
+}
+
+void setup_gpios()
+{
+    gpio_init(RED_LED_PIN);
+    gpio_set_dir(RED_LED_PIN, GPIO_OUT);
+    gpio_init(YELLOW_LED_PIN);
+    gpio_set_dir(YELLOW_LED_PIN, GPIO_OUT);
+    gpio_init(GREEN_LED_PIN);
+    gpio_set_dir(GREEN_LED_PIN, GPIO_OUT);
+
+    gpio_init(RED_RGB);
+    gpio_set_dir(RED_RGB, GPIO_OUT);
+    gpio_init(GREEN_RGB);
+    gpio_set_dir(GREEN_RGB, GPIO_OUT);
+    gpio_init(BLUE_RGB);
+    gpio_set_dir(BLUE_RGB, GPIO_OUT);
+
+    gpio_put(RED_LED_PIN, 1);
+    gpio_put(YELLOW_LED_PIN, 0);
+    gpio_put(GREEN_LED_PIN, 0);
+    gpio_put(RED_RGB, 1);
+    gpio_put(BLUE_RGB, 0);
+    gpio_put(GREEN_RGB, 0);
 }
